c59efc79236248f247baa534556c1435b1644988
Bei Li <bei@google.com>
2025-08-16 10:38:16 -0700

    Add a universal graph agent

---
+++ src/spanner_graph_agent/agents/graph_agent.py
@@ -0,0 +1,20 @@
+from google.adk.agents import LlmAgent
+
+from spanner_graph_agent.utils.prompts import (
+  GRAPH_AGENT_DEFAULT_DESCRIPTION,
+  GRAPH_AGENT_DEFAULT_INSTRUCTIONS,
+)
+
+class GraphAgent(LlmAgent):
+
+  def __init__(
+      self,
+      model: str,
+  ):
+    super().__init__(
+        model=model,
+        name='GraphAgent',
+        description=GRAPH_AGENT_DEFAULT_DESCRIPTION,
+        instruction=GRAPH_AGENT_DEFAULT_INSTRUCTIONS,
+        tools=[],
+    )
